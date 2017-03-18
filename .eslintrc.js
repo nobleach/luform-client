4cc50aaf721cfb1bf420143946af81a6e1beb866
Jim Wharton <jimwharton@overstock.com>
2017-03-18 10:03:29 -0600

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: 'airbnb-base',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // check if imports actually resolve
+  'settings': {
+    'import/resolver': {
+      'webpack': {
+        'config': 'build/webpack.base.conf.js'
+      }
+    }
+  },
+  // add your custom rules here
+  'rules': {
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      'optionalDependencies': ['test/unit/index.js']
+    }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'arrow-body-style': 0, // don't require braces in arrow function bodies
+    'arrow-parens': ["error", "as-needed", { "requireForBlockBody": false }], // no parens for arrow functions with arity 1
+    'comma-dangle': ["error", "never"], // no comma dangling please
+    'indent': [0, 4, {"SwitchCase": 1}] // TODO make this more strict
+  }
+}
---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+    <el-input placeholder="Please input" v-model="input"></el-input>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data() {
+        return {
+            input: ''
+        };
+    }
+};
+</script>
+
+<style lang="css">
+</style>
