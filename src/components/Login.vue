6f2264d6b3b7b8644c18178b2d7eabbdc1815772
Jim Wharton <jimwharton@overstock.com>
2017-03-18 11:55:18 -0600

    update loading status and disabled state

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,13 @@
 <template lang="html">
-    <el-input placeholder="Please input" v-model="input"></el-input>
+    <el-card class="box-card">
+        <h3>Please log in to continue</h3>
+        <form class="login-form">
+            <el-input class="login-input" placeholder="Email address" v-model="username"></el-input>
+            <el-input class="login-input" placeholder="Password" type="password" v-model="password"></el-input>
+            <el-button>Cancel</el-button>
+            <el-button type="primary" v-on:click="logIn" v-bind:disabled="username === ''" v-bind:loading="loading">{{loadingButtonText}}</el-button>
+        </form>
+    </el-card>
 </template>
 
 <script>
@@ -7,11 +15,28 @@ export default {
     name: 'Login',
     data() {
         return {
-            input: ''
+            username: '',
+            password: '',
+            loadingButtonText: 'Log in',
+            loading: false
         };
+    },
+    methods: {
+        logIn() {
+            this.loading = true;
+            this.loadingButtonText = 'Logging in';
+        }
     }
 };
 </script>
 
 <style lang="css">
+    .box-card {
+        margin: 0 auto;
+        width: 480px;
+    }
+
+    .login-input {
+        margin-bottom: 10px;
+    }
 </style>
