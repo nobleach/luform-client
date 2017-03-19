398bb2496751e72b02038825a1194ab56edc5d5b
Jim Wharton <jimwharton@overstock.com>
2017-03-18 21:21:22 -0600

    add form validation

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,15 +1,25 @@
 <template lang="html">
     <el-card class="box-card">
         <h3>Please register</h3>
-        <form class="registration-form">
-            <el-input class="registration-input" placeholder="First name" v-model="firstname"></el-input>
-            <el-input class="registration-input" placeholder="Last name" v-model="lastname"></el-input>
-            <el-input class="login-input" placeholder="Email address" v-model="username"></el-input>
-            <el-input class="login-input" placeholder="Password" type="password" v-model="password"></el-input>
-            <el-input class="login-input" placeholder="Retype your password" type="password" v-model="password"></el-input>
+        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="registration-form">
+            <el-form-item prop="firstname">
+                <el-input class="registration-input" placeholder="First name" v-model="ruleForm.firstname"></el-input>
+            </el-form-item>
+            <el-form-item prop="lastname">
+                <el-input class="registration-input" placeholder="Last name" v-model="ruleForm.lastname"></el-input>
+            </el-form-item>
+            <el-form-item prop="email">
+                <el-input class="registration-input" placeholder="Email address" v-model="ruleForm.email"></el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+                <el-input class="registration-input" placeholder="Password" type="password" v-model="ruleForm.password"></el-input>
+            </el-form-item>
+            <el-form-item prop="confirmPassword">
+                <el-input class="registration-input" placeholder="Retype your password" type="password" v-model="ruleForm.confirmPassword"></el-input>
+            </el-form-item>
             <el-button>Cancel</el-button>
-            <el-button type="primary" v-on:click="" v-bind:disabled="username === ''" v-bind:loading="loading">{{loadingButtonText}}</el-button>
-        </form>
+            <el-button type="primary" v-on:click="submitRegistration" v-bind:disabled="username === ''" v-bind:loading="loading">{{loadingButtonText}}</el-button>
+        </el-form>
     </el-card>
 </template>
 
@@ -18,16 +28,39 @@ export default {
     name: 'Register',
     data() {
         return {
-            loadingButtonText: 'Register'
+            ruleForm: {
+                firstname: '',
+                lastname: '',
+                email: '',
+                password: '',
+                confirmPassword: ''
+            },
+            rules: {
+                firstname: [
+                    { required: true, message: 'Please input first name', trigger: 'blur' },
+                    { min: 1, max: 15, message: 'Length should be 1 to 15', trigger: 'blur' }
+                ],
+                lastname: [
+                    { required: true, message: 'Please input last name', trigger: 'blur' },
+                    { min: 1, max: 15, message: 'Length should be 1 to 15', trigger: 'blur' }
+                ],
+                email: [
+                    { required: true, message: 'Please input your email address', trigger: 'blur' }
+                ]
+            },
+            loadingButtonText: 'Register',
+            loading: false
         };
     },
     methods: {
+        submitRegistration() {
+            this.loading = true;
+            this.loadingButtonText = 'Submitting...';
+            console.log(JSON.stringify(this.ruleForm));
+        }
     }
 };
 </script>
 
 <style lang="css">
-    .registration-input {
-        margin-bottom: 10px;
-    }
 </style>
