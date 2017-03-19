04b5b5244b4952f46b41e7f660a456b4bf8495ab
Jim Wharton <jimwharton@overstock.com>
2017-03-19 09:00:51 -0600

    add vuex; add store

--- src/main.js
+++ src/main.js
@@ -2,19 +2,23 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'normalize-css';
 import Vue from 'vue';
+import Vuex from 'vuex';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-default/index.css';
 import App from './App';
 import router from './router';
+import store from './store';
 
 Vue.config.productionTip = false;
 
 Vue.use(ElementUI);
+Vue.use(Vuex);
 
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
     router,
+    store,
     template: '<App/>',
     components: { App }
 });
